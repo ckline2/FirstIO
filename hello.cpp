290ca05fe2a21385142b837c4fb880ca4adb528e
ckline2 <ckline2@mail.csuchico.edu>
2018-09-05 16:44:34 -0700

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <string>
-
+/*Author:Charlie Kline
+ */
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Charlie Kline. What's yours?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
